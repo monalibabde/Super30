11f71e4be04456974919cdae99b63bed3fb69a1e
monalibabde <monali.babde@mitaoe.ac.in>
2025-04-01 19:45:49 +0530

    Log book generator

---
+++ OneDrive/Documents/NetBeansProjects/Logbook_generator/src/jTextArea2.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author monal
+ */
+class jTextArea2 {
+
+    static void setText(String string) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    static String getText() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
---
+++ OneDrive/Documents/NetBeansProjects/Logbook_generator/src/logbook_generator/Logbook_generator.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package logbook_generator;
+
+/**
+ *
+ * @author monal
+ */
+public class Logbook_generator {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
